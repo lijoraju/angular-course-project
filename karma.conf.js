28fd524adea48d0a8e4cabaf66a33ddacd271548
Lijo <lijoraju37@gmail.com>
2020-04-19 10:07:57 +0530

    course-project renamed to angular-course-project

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/course-project'),
+      dir: require('path').join(__dirname, './coverage/angular-course-project'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-project';
+  title = 'angular-course-project';
 }
